1a49173be37e5e936c6d2fe7b315a98238452615
suryajani283-droid <suryajani283@gmail.com>
2026-05-18 21:24:43 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ import WhatsAppButton from '../components/WhatsAppButton'
 
 export const metadata = {
   title: 'Naveen Academy Senior Secondary School | Best School in Chohtan, Barmer',
-  description: 'Naveen Academy offers quality education. Admissions Open 2024-25.',
+  description: 'Naveen Academy offers quality education. Admissions Open 2026-27.',
 }
 
 export default function RootLayout({ children }) {
