90dd20c67e507b88009894c8d174ed4ea1784a98
suryajani283-droid <suryajani283@gmail.com>
2026-05-18 13:35:45 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,30 +1,17 @@
 import './globals.css'
-import { Poppins, Hind } from 'next/font/google'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import WhatsAppButton from '../components/WhatsAppButton'
 
-const poppins = Poppins({ 
-  subsets: ['latin'],
-  weight: ['300', '400', '500', '600', '700'],
-  variable: '--font-poppins'
-})
-
-const hind = Hind({ 
-  subsets: ['devanagari'],
-  weight: ['300', '400', '500', '600', '700'],
-  variable: '--font-hind'
-})
-
 export const metadata = {
   title: 'Naveen Academy Senior Secondary School | Best School in Chohtan, Barmer',
-  description: 'Naveen Academy Senior Secondary School offers quality education from Primary to Senior Secondary. Admissions Open 2024-25.',
+  description: 'Naveen Academy offers quality education. Admissions Open 2024-25.',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${poppins.variable} ${hind.variable} font-sans`}>
+      <body>
         <Navbar />
         <main className="min-h-screen">
           {children}
