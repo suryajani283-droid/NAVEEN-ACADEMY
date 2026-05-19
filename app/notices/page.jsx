27ec897a58fa9e00226bf98f209167186055463c
suryajani283-droid <suryajani283@gmail.com>
2026-05-19 15:34:34 +0530

    Update page.jsx

--- app/notices/page.jsx
+++ app/notices/page.jsx
@@ -1,7 +1,7 @@
 'use client'
 import { useState, useEffect } from 'react'
 import { motion } from 'framer-motion'
-import { supabase } from '@/lib/supabase'
+import { supabase } from '../../lib/supabase';
 import {
   BellIcon,
   CalendarIcon,
