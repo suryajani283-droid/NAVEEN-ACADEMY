a14e534ae6659fceee0643aae901037ca44c34b9
suryajani283-droid <suryajani283@gmail.com>
2026-05-19 18:40:48 +0530

    Update page.jsx

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,5 +1,6 @@
-export const dynamic = 'force-dynamic';
+
 'use client'
+export const dynamic = 'force-dynamic';
 import { useState } from 'react'
 import { motion } from 'framer-motion'
 import { supabase } from '../../lib/supabase'
