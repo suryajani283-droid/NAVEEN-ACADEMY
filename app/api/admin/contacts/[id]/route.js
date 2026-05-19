10f6d021d7023a644e77b47b08f8fba386a4fb93
suryajani283-droid <suryajani283@gmail.com>
2026-05-19 19:30:35 +0530

    Update route.js

--- app/api/admin/contacts/[id]/route.js
+++ app/api/admin/contacts/[id]/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
-import { supabaseAdmin } from '../../../../lib/supabase';
-import { verifyAdminToken } from '../../../../lib/auth';
+import { supabaseAdmin } from '../../../../../lib/supabase';
+import { verifyAdminToken } from '../../../../../lib/auth';
 
 export async function DELETE(request, { params }) {
   try {
