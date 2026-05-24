4076c67cf4956332d454dfd5b3d9965fba847208
suryajani283-droid <suryajani283@gmail.com>
2026-05-24 18:06:42 +0530

    Delete useTeacherParams.js

--- app/teacher/useTeacherParams.js
+++
@@ -1,9 +0,0 @@
-import { useSearchParams } from 'next/navigation'
-
-export function useTeacherParams() {
-  const params = useSearchParams()
-  return {
-    teacherClass: params.get('class') || '',
-    subject: params.get('subject') || '',
-  }
-}
