e81c0c736925a90c0e5cdedb638a97d06107b038
suryajani283-droid <suryajani283@gmail.com>
2026-06-20 15:53:34 +0530

    Update ServiceWorkerRegister.jsx

--- components/ServiceWorkerRegister.jsx
+++ components/ServiceWorkerRegister.jsx
@@ -1,15 +1,20 @@
 'use client'
+
 import { useEffect } from 'react'
 
 export default function ServiceWorkerRegister() {
   useEffect(() => {
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/sw.js').then(
-        (registration) => console.log('SW registered:', registration.scope),
-        (err) => console.log('SW registration failed:', err)
-      )
+      navigator.serviceWorker
+        .register('/sw.js')
+        .then((registration) => {
+          console.log('Service Worker registered with scope:', registration.scope);
+        })
+        .catch((error) => {
+          console.error('Service Worker registration failed:', error);
+        });
     }
-  }, [])
+  }, []);
 
-  return null
+  return null; // This component doesn't render anything
 }
